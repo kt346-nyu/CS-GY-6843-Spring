57ba50b4ff1fc9948819065989df9587a86775f6
kt346-nyu <kt346@nyu.edu>
2026-02-14 23:43:36 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -9,8 +9,8 @@ def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   
   #Prepare a server socket
-  serverSocket.bind(("", port))
   #Fill in start
+  serverSocket.bind(("", port))
   serverSocket.listen(1)
   #Fill in end
 
@@ -18,29 +18,38 @@ def webServer(port=13331):
     #Establish the connection
     
     print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept() #Fill in start -are you accepting connections?    #Fill in end
+    #Fill in start -are you accepting connections?
+    connectionSocket, addr = serverSocket.accept()
+    #Fill in end
     
     try:
-      message = connectionSocket.recv(1024) #Fill in start -a client is sending you a message   #Fill in end
+
+      message = connectionSocket.recv()  #Fill in start -a client is sending you a message
+      #Fill in end
       filename = message.split()[1]
+
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:],  "r", encoding="utf-8"  )
+      f = open(filename[1:],"r", encoding="utf-8"  )
       content = f.read()
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
-      #Fill in start 
+      #Fill in start
+
       outputdata = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
       #Content-Type is an example on how to send a header as bytes. There are more!
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
       #Fill in end
                
       for i in f: #for line in file
-         file = open("helloworld.html")#Fill in start - append your html file contents #Fill in end
+        #Fill in start - append your html file contents
+        f.write(content)
+
+        #Fill in end
       #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command, do not send one line/item at a time!
-
       # Fill in start
-      connectionSocket.sendall(outputdata, file.encode())
+      connectionSocket.sendall(outputdata.encode(), content)
+
 
       # Fill in end
         
@@ -50,7 +59,8 @@ def webServer(port=13331):
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
       #Fill in start
-      outputdata = "<h1>HTTP/1.1 404 NOT FOUND\r\n\n<h1>404 Not Found</h1>"
+      outputdata = "HTTP/1.1 404 Not Found\r\n Content-Type: text/html; <h1>404 Not Found</h1>"
+
       connectionSocket.sendall(outputdata.encode())
       #Fill in end
 
