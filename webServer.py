f46c222e619a8cfd77a102e600071f2fa88e1275
kt346-nyu <kt346@nyu.edu>
2026-02-14 21:53:42 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -10,7 +10,6 @@ def webServer(port=13331):
   
   #Prepare a server socket
   serverSocket.bind(("", port))
-  
   #Fill in start
   serverSocket.listen(1)
   #Fill in end
@@ -24,27 +23,19 @@ def webServer(port=13331):
     try:
       message = connectionSocket.recv(1024) #Fill in start -a client is sending you a message   #Fill in end
       filename = message.split()[1]
-
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
       f = open(filename[1:],  "r", encoding="utf-8"  )
       content = f.read()
-
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
       #Fill in start 
-              
+      outputdata = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
       #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata = "HTTP/1.1 200 OK\r\n"
-      outputdata += "Content-Type: text/html; charset=UTF-8\r\n"
-      outputdata += "\r\n"
-
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
- 
       #Fill in end
                
       for i in f: #for line in file
-         file = f.write(i + '\n') #Fill in start - append your html file contents #Fill in end
-
+         file = open("helloworld.html")#Fill in start - append your html file contents #Fill in end
       #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command, do not send one line/item at a time!
 
@@ -59,7 +50,7 @@ def webServer(port=13331):
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
       #Fill in start
-      outputdata = "HTTP/1.1 404 NOT FOUND\r\n\n<h1>404 Not Found</h1>"
+      outputdata = "<h1>HTTP/1.1 404 NOT FOUND\r\n\n<h1>404 Not Found</h1>"
       connectionSocket.sendall(outputdata.encode())
       #Fill in end
 
